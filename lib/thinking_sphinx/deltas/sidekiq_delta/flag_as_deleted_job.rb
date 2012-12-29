db4f9d2a5c3cd5dcf70b0e30665564751e8d3259
Pat Allan <pat@freelancing-gods.com>
2012-12-29 19:56:45 +1100

    Update for latest TS edge support.

--- lib/thinking_sphinx/deltas/sidekiq_delta/flag_as_deleted_job.rb
+++ lib/thinking_sphinx/deltas/sidekiq_delta/flag_as_deleted_job.rb
@@ -6,9 +6,11 @@ class ThinkingSphinx::Deltas::SidekiqDelta::FlagAsDeletedJob
   sidekiq_options unique: true, retry: true, queue: 'ts_delta'
 
   def perform(index, document_id)
-    ThinkingSphinx::Configuration.instance.connection.query(
-      Riddle::Query.update(index, document_id, :sphinx_deleted => true)
-    )
+    ThinkingSphinx::Connection.pool.take do |connection|
+      connection.query(
+        Riddle::Query.update(index, document_id, :sphinx_deleted => true)
+      )
+    end
   rescue Mysql2::Error => error
     # This isn't vital, so don't raise the error
   end
